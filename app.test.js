e1956d3afefd8267054e1d2cff02453f7f7d6931
xelva <xelvacodes@gmail.com>
2022-04-20 09:56:06 -0500

    added manual testing

---
+++ app.test.js
@@ -0,0 +1,49 @@
+const { forEach, map } = require('./app.js');
+const assert = require('assert');
+
+//helper function
+const test = (desc, fn) => {
+    console.log('------', desc);
+    try {
+        fn();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//forEach
+test('test the forEach function', () =>{
+    let sum = 0;
+    forEach([1,2,3], value => {
+    sum += value;
+    });
+
+    assert.strictEqual(sum, 6, 'Expected to equal 6');
+
+    //before useing assert
+    /* if (sum !== 6 ) {
+        throw new Error('Expected sum to equal 6')
+    }; */
+});
+
+
+//map
+test('test the map function', () => {
+    const result = map([1,2,3], value => {
+        return value * 2
+    });
+    assert.strictEqual(result[0], 2);
+    assert.strictEqual(result[1], 4);
+    assert.strictEqual(result[2], 6);
+
+    //before using assert
+  /*   if (result[0] !== 2) {
+        throw new Error(`Expected to find 2, but found ${result[0]}`)
+    }
+    if (result[1] !== 4) {
+        throw new Error(`Expected to find 4, but found ${result[1]}`)
+    }
+    if (result[2] !== 6) {
+        throw new Error(`Expected to find 6, but found ${result[2]}`)
+    } */
+});
